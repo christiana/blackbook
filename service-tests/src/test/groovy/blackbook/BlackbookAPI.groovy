f0eef51136d4a6d426314a8350ae93411a43271b
geipette <geir.pettersen@finn.no>
2016-07-12 16:23:56 +0200

    Added failing test for /trips/[doesnotexist]

--- service-tests/src/test/groovy/blackbook/BlackbookAPI.groovy
+++ service-tests/src/test/groovy/blackbook/BlackbookAPI.groovy
@@ -1,6 +1,8 @@
 package blackbook
 
+import groovyx.net.http.HttpResponseException
 import groovyx.net.http.RESTClient
+import net.sf.json.JSON
 import spock.lang.Specification
 import spock.lang.Stepwise
 
@@ -25,6 +27,34 @@ class BlackbookAPI extends Specification {
         then:
           resp.contentType == 'application/json'
 
+    }
+
+    def "GET /trips returns an empty list"() {
+        when:
+          def resp = client.get path: '/trips'
+
+        then:
+          resp.data instanceof List
+          resp.data.size() == 0
+
+    }
+
+    def "GET none existing trip returns 404 NOT FOUND"() {
+        when:
+          client.get path: '/trips/1'
+
+        then:
+          def ex = thrown(HttpResponseException)
+          ex.response.status == 404
+    }
+
+    def "GET none existing trip returns application/json content-type"() {
+        when:
+          client.get path: '/trips/1'
+
+        then:
+          def ex = thrown(HttpResponseException)
+          ex.response.contentType == 'application/json'
     }
 
 }
